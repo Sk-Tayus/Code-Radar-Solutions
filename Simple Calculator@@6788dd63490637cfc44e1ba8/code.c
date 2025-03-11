f92a24f08a500b341e4b3b420dcee050b3b815b4
Sk-Tayus <sktayusuddin@gmail.com>
2025-03-11 10:37:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,7 +13,7 @@ int main() {
         printf("%d",a*b);
     }
     else if (operators == '/') {
-        printf("%d",(float)a/b);
+        printf("%d",a/b);
     }
     else {
         printf("Yoyo");
